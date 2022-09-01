ecf59efe7b1ac64be80a2ba2a186910c227cbccc
XR Dev3 <armandsumo@gmail.com>
2022-09-01 16:40:49 +0200

    update version

--- agora-rtc/flutter_service_worker.js
+++ agora-rtc/flutter_service_worker.js
@@ -19,9 +19,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "2b90a13bb215515aa869a7149107d6f1",
-"/": "2b90a13bb215515aa869a7149107d6f1",
-"main.dart.js": "74b89cd8137949f45c73f2b0ca887687",
+"index.html": "0299d69bc72148301348f28050a6c953",
+"/": "0299d69bc72148301348f28050a6c953",
+"main.dart.js": "38d205e7f678d3c9b9a2ccd1a7cca0b2",
 "manifest.json": "3a5b6095130313cef6dbb5db78bc1986",
 "version.json": "c72dd9007e6fa6ce5029cbe514299506"
 };
