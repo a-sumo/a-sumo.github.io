5ecad3dc53d0cca69b28e7934b1e9b9726c9de4f
a-sumo <armandsumo@gmail.com>
2025-04-13 22:26:37 +0200

    remove post

--- .astro/assets.mjs
+++ .astro/assets.mjs
@@ -1,5 +1,1 @@
-
-import __ASTRO_IMAGE_IMPORT_Z1VnB86 from './public/images/dragon-bounce.png?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fdragon-bounce.md';
-import __ASTRO_IMAGE_IMPORT_1hVFX0 from './public/thumbnails/dragon-bounce.png?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fdragon-bounce.md';
-export default new Map([["./public/images/dragon-bounce.png?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fdragon-bounce.md", __ASTRO_IMAGE_IMPORT_Z1VnB86], ["./public/thumbnails/dragon-bounce.png?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fdragon-bounce.md", __ASTRO_IMAGE_IMPORT_1hVFX0]]);
-		
+export default new Map();
