76d1dd191be41e2f5513743ff75abada06c46485
a-sumo <armandsumo@gmail.com>
2025-08-20 18:13:35 +0200

    set default theme to light

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,7 +9,7 @@ function withOpacity(variableName) {
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: ["selector", "[data-theme='dark']"],
+  darkMode: ["selector", "[data-theme='light']"],
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     screens: {
